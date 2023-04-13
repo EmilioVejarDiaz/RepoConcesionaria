42b313282fb41e290d1afa705292ecef874b164e
EmilioVejarDiaz <20213tn091@utez.edu.mx>
2023-04-13 13:29:24 -0600

    Proyecto

---
+++ src/main/java/mx/utez/edu/Concesionaria2/config/ConfiguracionMongoDB.java
@@ -0,0 +1,44 @@
+package mx.utez.edu.Concesionaria2.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "mx.utez.edu.Concesionaria2.repository")
+public class ConfiguracionMongoDB {
+
+    @Bean
+    public MongoClient mongoClient() {
+        //return MongoClients.create("mongodb://localhost:27017/Concesionaria");
+
+
+            return MongoClients.create("mongodb+srv://Linkelink:20213Tn091@example-01.tkqv1i9.mongodb.net/test");
+
+
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongoClient(), "Concesionaria");
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                    .allowCredentials(false)
+                    .maxAge(3600);
+        }
+    }
+
+}
